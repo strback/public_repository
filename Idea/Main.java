229d30ae6d3ca1e40fe244aa5f43dbce47fa6abb
zaki_pro <1480632161_pro@qq.com>
2020-02-26 18:37:25 +0800

    第一个次创建Idea

---
+++ Idea/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+	
+	public static void main(String[] args){
+		
+		System.out.println("我是Java。");
+		System.out.println("我已经发展了20多年了。");	
+		System.out.println("我的生态非常庞大，我的社区十分活跃。");	
+	}
+	
+}
